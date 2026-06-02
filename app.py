17d6dda30311fcddb01bdda8aa4df4a310ea4cf3
yh <h.yuan@zofre.de>
2026-06-02 10:05:37 +0200

    feat: activate register and login by adding postgresql

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ gravatar = Gravatar(app,
 class Base(DeclarativeBase):
     pass
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI") or os.environ.get("DATABASE_URL") or "sqlite:///posts.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
